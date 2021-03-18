231fad4265792c04d9ae3c4d36e22c89da671ee1
Joel G <joel@joelgonzaga.com>
2021-03-18 15:02:31 -0700

    added a basic health test for root. Works.

--- backend/mono/src/server.ts
+++ backend/mono/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import {Request, Response} from 'express';
 
 
 const app = express()
 const port = process.env.PORT || 8080
 
+app.get('/health', async (req: Request, res: Response) => {
+    res.status(200).send('Root of API is up');
+})
 app.listen(port, () => {
     console.log(`Server is Running on port ${port}`)
     console.log(`press CTRL+C to stop server`)
