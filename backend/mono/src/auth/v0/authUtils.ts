79622a7b903f8a0ac0812321d8eb97dfb884f862
Joel G <joel@joelgonzaga.com>
2021-03-18 18:59:36 -0700

    added, but untested, authentication functionality.

--- backend/mono/src/auth/v0/authUtils.ts
+++ backend/mono/src/auth/v0/authUtils.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express'
-import { decode } from 'jsonwebtoken'
-import { JwtPayload } from '../interfaces';
+import { decode, verify } from 'jsonwebtoken'
+import { JwtPayload, Jwt } from '../interfaces'
+import { config } from '../../config'
+import Axios from 'axios'
 
 export function getUserId(req: Request) {
     const authorization = req.headers.authorizataion as string
@@ -15,4 +17,44 @@ export function parseUserId(jwtToken: string): string {
     const decodedJwt = decode(jwtToken) as JwtPayload
     
     return decodedJwt.sub
+  }
+
+  async function verifyToken(authHeader: string): Promise<JwtPayload> {
+    const token = getToken(authHeader)
+    const jwt: Jwt = decode(token, { complete: true }) as Jwt
+    const rawCert: string = await matchToKey(jwt.header.kid)
+    const cert = stringToPEM(rawCert)
+    return verify(token, cert, { algorithms: ['RS256']}) as JwtPayload
+  }
+  
+  function getToken(authHeader: string): string {
+    if (!authHeader) throw new Error('No authentication header')
+  
+    if (!authHeader.toLowerCase().startsWith('bearer '))
+      throw new Error('Invalid authentication header')
+    const split = authHeader.split(' ')
+    const token = split[1]
+    return token
+  }
+  
+  async function matchToKey(kid: any): Promise<string> {
+    try{
+      const actualKeys = await Axios.get(config.authUrl)
+      const signerKey = actualKeys.data.keys.filter((key: { [x: string]: string }) => {key[kid] === kid})[0] || actualKeys.data.keys[0]
+      const x5cKey: string = signerKey.x5c[0]
+      if(!x5cKey){
+        throw new Error(`Unable to Match any Keys. x5cKey not extracted.`)
+      }
+  
+      return x5cKey
+    }catch(e){
+      return ''
+    }
+    
+  }
+  
+  function stringToPEM(cert: string): string {
+    cert = cert.match(/.{1,64}/g).join('\n');
+    cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+    return cert;
   }
