1b0be2a9aaa3b0c2f7e867422da54693348f891c
Joel G <joel@joelgonzaga.com>
2022-01-15 12:28:14 -0800

    Added Authguard for Campaigns.

--- backend/mono/src/01Http/v0/campaign.router.ts
+++ backend/mono/src/01Http/v0/campaign.router.ts
@@ -1,4 +1,6 @@
 import {Router, Request, Response} from 'express';
+import { requireAuth } from '../../auth/v0/require.auth';
+
 
 const router: Router = Router()
 
@@ -6,25 +8,25 @@ router.get('/health', async (req: Request, res: Response) => {
     res.status(200).send('Campaign API is up');
 })
 
-router.post('/', async (req: Request, res: Response) => {
+router.post('/', requireAuth, async (req: Request, res: Response) => {
     res.status(200).send({reply:'POST method is up', calandar: 'Campaign'});
 })
 
-router.get('/:id', async (req: Request, res: Response) => {
+router.get('/:id', requireAuth, async (req: Request, res: Response) => {
     const id = req.params.id
     res.status(200).send({reply: `GET ONE is up with ID of ${id}`, calandar: 'Campaign'})
 })
 
-router.get('/', async (req: Request, res: Response) => {
+router.get('/', requireAuth, async (req: Request, res: Response) => {
     res.status(200).send({reply:'GET ALL method is up', calandar: 'Campaign'});
 })
 
-router.patch('/:id', async (req: Request, res: Response) => {
+router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
     const id = req.params.id
     res.status(200).send({reply: `PATCH is up with ID of ${id},`, calandar: 'Campaign'})
 })
 
-router.delete('/:id', async (req: Request, res: Response) => {
+router.delete('/:id', requireAuth,async (req: Request, res: Response) => {
     const id = req.params.id
     res.status(200).send({reply: `DELETE is up with ID of ${id}`, calandar: 'Campaign'})
 })
