a149beb192a876449625d4ef5342443f32c507af
Joel G <joel@joelgonzaga.com>
2022-01-15 20:44:31 -0800

    adding user interfaces.

--- backend/mono/src/interfaces/user.interface.ts
+++ backend/mono/src/interfaces/user.interface.ts
@@ -1,11 +1,11 @@
-export interface user {
+export interface User {
     _id?: String,
     _authID: String,
     name: String,
     tier: Number
 }
 
-export interface userPatch {
+export interface UserPatch {
     name: String,
     tier: Number
 }
