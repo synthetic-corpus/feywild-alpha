9ca7cded01a0c979af2786f5bf987d9dcbd4ab89
Joel G <joel@joelgonzaga.com>
2022-02-13 18:10:24 -0800

    corrected error messages.

--- backend/mono/src/03data/harptos.mongo.ts
+++ backend/mono/src/03data/harptos.mongo.ts
@@ -25,15 +25,15 @@ export async function retrieveHarptos(userId: String, harptosId: String): Promis
         const thisHarptos = await HarptosModel.findById(harptosId)
         if(thisHarptos){
             if (userId === thisHarptos._user_id.toString()){
-                reply = {code: 200,message: "retrieved character tent",data: thisHarptos}
+                reply = {code: 200,message: "retrieved harptos!",data: thisHarptos}
             }else{
                 reply = {code: 403,message: "You are not authorized to view this resource!"}
             }
         }else{
-            reply = {code: 404,message: "No Tent found!"}
+            reply = {code: 404,message: "No Harptos found!"}
         }
     }catch(e){
-        console.log(`Error on retrieving tent at database layer for tent: ${harptosId} \n ${e}`)
+        console.log(`Error on retrieving tent at database layer for harptos: ${harptosId} \n ${e}`)
         reply = {code: 500,message: "Internal Server Error"}
     }
     return reply
@@ -45,31 +45,31 @@ export async function retrieveAllHarptos(userId: String): Promise<HttpReplyMessa
         const query = {_user_id: userId}
         const theseCalendars = await HarptosModel.find(query)
         if(theseCalendars){
-            reply = {code: 202,message: 'Found Tents',data: theseCalendars}
+            reply = {code: 202,message: 'Found Harptos',data: theseCalendars}
         }else{
             reply = {code: 404,message: "No Calendar found!",data: []}
         }
     }catch(e){
-        console.log(`Error on retrieving Calendar at database layer for tent: ${userId} \n ${e}`)
+        console.log(`Error on retrieving Calendar at database layer for Harptos: ${userId} \n ${e}`)
         reply = {code: 500,message: "Internal Server Error"}
     }
     return reply
 }
 
-export async function updateHarptos(userId: String, harptosId: String, tentPatch: HarptosUpdate): Promise<HttpReplyMessage>{
+export async function updateHarptos(userId: String, harptosId: String, harptosPatch: HarptosUpdate): Promise<HttpReplyMessage>{
     let reply: HttpReplyMessage;
     const query = {_id: harptosId,_user_id: userId}
     try{
-        const updatedHarptos = await HarptosModel.findOneAndUpdate(query,tentPatch)
+        const updatedHarptos = await HarptosModel.findOneAndUpdate(query,harptosPatch)
         if (updatedHarptos){
             const data = updatedHarptos
-            reply = {code:201, message: "Updated Tent",data: data}
+            reply = {code:201, message: "Updated Harptos",data: data}
         }else{
-            console.log(`Could not find tent: ${harptosId} with user: ${userId}. Possible auth issue!`)
-            reply = {code:400, message: 'Unable to find tent or match with user'}
+            console.log(`Could not find Harptos: ${harptosId} with user: ${userId}. Possible auth issue!`)
+            reply = {code:400, message: 'Unable to find calendar or match with user'}
         }
     }catch(e){
-        console.log(`Database Layer error: Could not retrieve tent: ${harptosId} with user: ${userId} \n ${e}`)
+        console.log(`Database Layer error: Could not retrieve Harptos: ${harptosId} with user: ${userId} \n ${e}`)
         reply = {code: 500, message: "Internal Server Error"}
     }
     return reply
@@ -84,11 +84,11 @@ export async function deleteHarptos(userId: String, harptosId: String): Promise<
             const data = deletedHarptos
             reply = {code:201, message: "Removed Tent Tent",data: data}
         }else{
-            console.log(`Could not find tent: ${harptosId} with user: ${userId}. Possible auth issue!`)
+            console.log(`Could not find harptos to deleted: ${harptosId} with user: ${userId}. Possible auth issue!`)
             reply = {code:400, message: 'Unable to find calendar or match with user'}
         }
     }catch(e){
-        console.log(`Database Layer error: Could not Deleted calendar: ${harptosId} with user: ${userId} \n ${e}`)
+        console.log(`Database Layer error: Could not Delete calendar: ${harptosId} with user: ${userId} \n ${e}`)
         reply = {code: 500, message: "Internal Server Error"}
     }
     return reply
