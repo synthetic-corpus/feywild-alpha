6b023b6650a6581ba085a05146558ac6cf701dfb
Joel G <joel@joelgonzaga.com>
2022-02-23 20:43:39 -0800

    Encounter database layer completed.

---
+++ backend/mono/src/03data/schemas/encounter.schema.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const encounterNPC = new mongoose.Schema({
+    name: {type: String, required: true},
+    initiative: {types: String, required: true},
+    ac: {type: Number},
+    notes: {type: String}
+},{_id: false})
+
+const encounterSchema = new mongoose.Schema({
+    _user_id: {type: ObjectId, required: true},
+    _campaign_id: {type: ObjectId},
+    name: {type: String, required: true},
+    npcs: {type: [encounterNPC], default: []}
+})
+
+export const EncounterModel = mongoose.model('Encounter',encounterSchema)
---
+++ backend/mono/src/interfaces/encounter.interface.ts
@@ -0,0 +1,22 @@
+interface Npcs {
+    name: string,
+    initiative: number,
+    ac?: number,
+    notes?: string
+}
+
+export interface EncounterHttp {
+    _campaign_id?: string,
+    name: string,
+    npcs?: Npcs[]
+}
+
+export interface Encounter extends EncounterHttp {
+    _user_id: string
+}
+
+export interface EncounterPatch {
+    _campaign_id?:string
+    name?: string,
+    npcs?: Npcs[]
+}
