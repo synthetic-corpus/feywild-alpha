d32eea43309e87c97984fb186eb7856d1c949a92
Joel G <joel@joelgonzaga.com>
2022-03-27 13:37:33 -0700

    added outline for campaign retrieval.

--- webfront/versions/fey-front-v0/src/app/services/http/campaign-http.service.ts
+++ webfront/versions/fey-front-v0/src/app/services/http/campaign-http.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { CampaignHttp, CampaignPatch } from './interfaces/campaign.interfaces';
+import { HttpClient } from '@angular/common/http';
+import  * as config from '../../../../auth_config.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampaignHttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createCampaign(newCampaign: CampaignHttp ){
+    return this.http.post(`${config.apiUri}/campaign`,newCampaign)
+  }
+
+  retrieveCampaign(id: string){
+    return this.http.get(`${config.apiUri}/campaign/${id}`)
+  }
+
+  retrieveCampaigns(){
+    return this.http.get(`${config.apiUri}/campaign/`)
+  }
+
+  updateCampaign(id: string, patch: CampaignPatch){
+    return this.http.patch(`${config.apiUri}/campaign/${id}`, patch)
+  }
+
+  deleteCampaign(id: string){
+    return this.http.delete(`${config.apiUri}/campaign${id}`)
+  }
 }
