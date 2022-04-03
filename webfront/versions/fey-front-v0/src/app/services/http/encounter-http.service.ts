3f37da513e40369508e508e47662e9bf41df9e4b
Joel G <joel@joelgonzaga.com>
2022-04-03 14:51:40 -0700

    enabled CORS headers.

--- webfront/versions/fey-front-v0/src/app/services/http/encounter-http.service.ts
+++ webfront/versions/fey-front-v0/src/app/services/http/encounter-http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { EncounterHttp, EncounterPatch } from './interfaces/encounter.interfaces';
 import { environment as env } from '../../../environments/environment';
+import { CORS } from './headers';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,22 @@ export class EncounterHttpService {
   constructor(private http: HttpClient) { }
 
   createEncounter(newEncounter: EncounterHttp ){
-    return this.http.post(`${env.auth.apiUri}/encounter/`,newEncounter)
+    return this.http.post(`${env.auth.apiUri}/encounter/`,newEncounter, CORS)
   }
 
   retrieveEncounter(id: string){
-    return this.http.get(`${env.auth.apiUri}/encounter/${id}`)
+    return this.http.get(`${env.auth.apiUri}/encounter/${id}`, CORS)
   }
 
   retrieveEncounters(){
-    return this.http.get(`${env.auth.apiUri}/encounter/`)
+    return this.http.get(`${env.auth.apiUri}/encounter/`, CORS)
   }
 
   updateEncounter(id: string, patch: EncounterPatch){
-    return this.http.patch(`${env.auth.apiUri}/encounter/${id}`, patch)
+    return this.http.patch(`${env.auth.apiUri}/encounter/${id}`, patch, CORS)
   }
 
   deleteEncounter(id: string){
-    return this.http.delete(`${env.auth.apiUri}/encounter/${id}`)
+    return this.http.delete(`${env.auth.apiUri}/encounter/${id}`, CORS)
   }
 }
