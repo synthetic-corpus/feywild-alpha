faf6dd38a77f5d5204c8ae94f044a15c2dea570f
Joel G <joel@joelgonzaga.com>
2022-04-16 14:23:19 -0700

    added basic routing.

--- webfront/versions/fey-front-v0/src/app/app-router.module.ts
+++ webfront/versions/fey-front-v0/src/app/app-router.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EncountersComponent } from './components/encounters/encounters.component';
+import { TentsComponent } from './components/tents/tents.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'encounters', component: EncountersComponent},
+  { path: 'tents', component: TentsComponent}
 ]
 
 @NgModule({
