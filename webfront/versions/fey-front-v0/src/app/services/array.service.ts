92167e1bd9f08e6226298ca4c0b8f02b4f38a948
Joel G <joel@joelgonzaga.com>
2022-04-30 17:47:57 -0700

    new idea. Create an array service. Once per component. Not provided at root.

---
+++ webfront/versions/fey-front-v0/src/app/services/array.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ArrayService {
+  // Expect to get objects with names like "web_element_id in them."
+  // User for get their index and possibley mutating arrays.
+  myArray!: any[]
+  constructor(
+  ) {}
+
+  getArray(){
+    return this.myArray
+  }
+
+  getElementIndex(object){
+    return this.myArray.findIndex((element)=>object.web_element_id === element.web_element_id)
+  }
+
+  replaceElement(object){
+    const index = this.getElementIndex(object)
+    this.myArray.splice(index,1,object)
+  }
+
+  deleteElement(object){
+    const index = this.getElementIndex(object)
+    this.myArray.splice(index,1)
+  }
+
+  duplicateElement(object){
+    const index = this.getElementIndex(object)
+    this.myArray.splice(index,0,object)
+  }
+}
