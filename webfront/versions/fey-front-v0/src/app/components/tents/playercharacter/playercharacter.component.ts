cf9153c3051b0e269fca1e0bf73c632982dfdaf6
Joel G <joel@joelgonzaga.com>
2022-05-15 12:50:35 -0700

    created the single player character element.

--- webfront/versions/fey-front-v0/src/app/components/tents/playercharacter/playercharacter.component.ts
+++ webfront/versions/fey-front-v0/src/app/components/tents/playercharacter/playercharacter.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { SanitizeStringsService } from 'src/app/services/sanitize-strings.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TentWeb } from '../../../interfaces/tent.interface'
 
 @Component({
   selector: 'app-playercharacter',
@@ -6,10 +9,61 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./playercharacter.component.css']
 })
 export class PlayercharacterComponent implements OnInit {
-  @Input() player_id!: string
-  constructor() { }
+  @Output() playerChanged = new EventEmitter<TentWeb>()
+  @Output() playerDeleted = new EventEmitter<String>
+
+  @Input() maxed_out!: boolean
+  @Input() web_element_id!: string
+  @Input() player!: string
+  @Input() character!: string
+  @Input() initiative!: number
+  @Input() passive_perception: number
+  @Input() spell_dc: number
+  @Input() ac!: number
+  @Input() notes!: string
+
+  editting: boolean = false
+  playerForm: FormGroup
+
+  constructor(
+    private sanitizeString: SanitizeStringsService
+  ) { }
 
   ngOnInit(): void {
+    this.playerForm = new FormGroup({
+      'playerFC': new FormControl(this.player, [Validators.required,this.sanitizeString.mustHaveLetters()]),
+      'characterFC': new FormControl(this.character, [Validators.required,this.sanitizeString.mustHaveLetters()]),
+      'initiativeFC': new FormControl(this.initiative,[Validators.required]),
+      'passive_perceptionFC': new FormControl(this.passive_perception),
+      'spell_dcFC': new FormControl(this.spell_dc),
+      'armorFC': new FormControl(this.ac),
+      'notesFC': new FormControl(this.notes)
+    })
+  }
+
+  toggleEdit(){
+    this.editting ? this.editting = false : this.editting = true
+  }
+
+  onSubmit(){
+    this.player = this.sanitizeString.sanitize(this.playerForm.value.playerFC)
+    this.character = this.sanitizeString.sanitize(this.playerForm.value.characterFC)
+    this.notes = this.sanitizeString.sanitize(this.playerForm.value.notes)
+    this.playerChanged.emit({
+      web_element_id: this.web_element_id,
+      player: this.player,
+      character: this.character,
+      initiative: this.initiative,
+      passive_perception: this.passive_perception,
+      spell_dc: this.spell_dc,
+      ac: this.ac,
+      notes: this.notes
+    })
+    this.toggleEdit()
+  }
+
+  onDeleteMe(){
+    this.playerDeleted.emit(this.web_element_id)
   }
 
 }
